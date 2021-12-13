7c41bdfec9efaad019aac9caa008cab8f13d0435
alex_sdk@hotmail.ca <Cev-Dev@users.noreply.github.com>
2021-12-12 20:45:26 -0500

    snake game with python

---
+++ SnakeGame/scoreboard.py
@@ -0,0 +1,24 @@
+from os import scandir
+from turtle import Turtle, screensize
+
+ALIGNMENT = 'center'
+FONT = ('Arial', 24, 'normal')
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color('white')
+        self.hideturtle()
+        self.penup()
+        self.score = 0
+        self.goto(x=0, y= 270)
+        self.write(f"Score = {self.score}", align=ALIGNMENT, font=FONT)
+
+    def Refresh_score(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score = {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write('Game Over', align=ALIGNMENT, font=FONT)
