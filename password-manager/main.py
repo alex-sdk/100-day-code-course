472b36428ea6f2264afdc7670196662832d82a86
Cev <Cev-Dev@users.noreply.github.com>
2021-12-23 21:26:13 -0500

    Password Manager GUI app

---
+++ password-manager/main.py
@@ -0,0 +1,66 @@
+from tkinter import *
+from tkinter import messagebox
+from random  import choice, randint, shuffle
+
+def save():
+    website = website_entry.get()
+    email = email_user_entry.get()
+    password = password_entry.get()
+
+    if website == '' or password == '' or email == '':
+        messagebox.showwarning(message='You have left an empty field')
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"Are you sure you would like to save this data within data.txt?")
+        if is_ok:
+            with open('data.txt', 'a') as data_file:
+                data_file.write(f"{website}|{email}|{password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0 , END)
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+            'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for char in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for char in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for char in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = ''.join(password_list)
+    password_entry.insert(0, password)
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+lock_img = PhotoImage(file='logo.png')
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(row=0, column=1)
+
+website_label = Label(text='Website')
+website_label.grid(row=1, column=0)
+email_user_label = Label(text='Email/Username')
+email_user_label.grid(row=2, column=0)
+password_label = Label(text='Password')
+password_label.grid(row=3, column=0)
+
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+email_user_entry = Entry(width=35)
+email_user_entry.grid(row=2, column=1, columnspan=2)
+email_user_entry.insert(0, 'email@email.com')
+password_entry = Entry(width=18)
+password_entry.grid(row=3, column=1)
+
+gen_password_button = Button(text='Generate Password', command=generate_password)
+gen_password_button.grid(row=3, column=2)
+add_button = Button(width= 32, text='Add', command=save)
+add_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
